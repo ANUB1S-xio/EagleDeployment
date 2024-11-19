7e7fa58d36fba4a6b326b22afb14cc35dc955801
Hunter R <hunterroberts1@my.unt.edu>
2024-11-18 18:08:38 -0600

    ssh connection issue was becasue the ssh_user had a capital H, when it needed to be a lower case h.....

--- EagleDeploy_CLI/sshutils/sshutils.go
+++ EagleDeploy_CLI/sshutils/sshutils.go
@@ -2,6 +2,7 @@ package sshutils
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -11,18 +12,30 @@ import (
 
 // Function to establish an SSH connection
 func ConnectSSH(host, user, password string, port int) (*ssh.Client, error) {
+	// Logging input parameters for verbose mode
+	log.Printf("Connecting to SSH server: %s on port: %d as user: %s", host, port, user)
+
 	config := &ssh.ClientConfig{
 		User: user,
 		Auth: []ssh.AuthMethod{
 			ssh.Password(password),
 		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // WARNING: Not recommended for production
 	}
+
 	address := fmt.Sprintf("%s:%d", host, port)
+	log.Printf("Attempting to dial %s...", address)
+
 	client, err := ssh.Dial("tcp", address, config)
 	if err != nil {
+		// Logging detailed error information
+		log.Printf("Failed to connect to SSH server: %s. Error: %v", address, err)
 		return nil, fmt.Errorf("failed to connect to %s: %w", address, err)
 	}
+
+	// Logging successful connection
+	log.Printf("Successfully connected to SSH server: %s", address)
+
 	return client, nil
 }
 
