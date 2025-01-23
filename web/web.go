09b2a7614c11a9a632b784a41833a05475f8f0a6
anon <cthulhu@cthulhu.Bermuda-Dark>
2025-01-22 20:27:05 -0500

    Integrated web server into main with concurrency

--- web/web.go
+++ web/web.go
@@ -7,13 +7,16 @@ import (
 
 // StartWebServer starts the web server with TLS on a predefined port
 func StartWebServer() {
+	//handle request to root URL
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "EagleDeployment Web Interface is running securely with TLS!")
-		// Add more routes as needed
+		fmt.Fprintln(w, "EagleDeployment Web Interface is running...")
+
+		// Add more routes as needed (api endpoints)
+
 	})
 
-	port := 8443
-	fmt.Printf("TLS web server running at https://localhost:%d\n", port)
+	port := 8080
+	fmt.Printf("EagleDeployment GUI running at https://localhost:%d\n", port)
 
 	// Use a self-signed certificate (replace with paths to your cert and key files)
 	certFile := "cert.pem" // Path to the certificate file
@@ -21,6 +24,6 @@ func StartWebServer() {
 
 	err := http.ListenAndServeTLS(fmt.Sprintf(":%d", port), certFile, keyFile, nil)
 	if err != nil {
-		fmt.Printf("Failed to start web server with TLS: %v\n", err)
+		fmt.Printf("Web Interface failed to start: %v\n", err)
 	}
 }
